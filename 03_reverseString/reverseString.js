c67a9b6f75d582cbb677de070c231c6a8665959f
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-17 12:18:49 -0600

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    let phraseAsArray = Array.from(phrase);
+    let backwardsPhrase = '';
 
+    while(phraseAsArray.length) {
+        backwardsPhrase += phrase.pop();
+    }
 };
 
 // Do not edit below this line
