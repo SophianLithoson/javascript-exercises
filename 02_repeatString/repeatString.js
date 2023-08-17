85b96b4e64577170ad6e350383d1d150d55e87b4
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-17 11:41:29 -0600

    exercise 2 test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, numRepetitions) {
+    let result = "";
+    for(let i = 0; i < numRepetitions; i++) {
+        result += phrase;
+    }
+    return result;
 };
 
 // Do not edit below this line
