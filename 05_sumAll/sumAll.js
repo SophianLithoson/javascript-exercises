2bc6e0e9a7155b79294edd9ce1be460382c0351b
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-17 16:12:09 -0600

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(firstInt, lastInt) {
+    if (firstInt===NaN || lastInt===NaN || firstInt < 0 || lastInt < 0)
+        return 'ERROR';
 
+    if (firstInt > lastInt) {
+        let x = lastInt;
+        firstInt = lastInt;
+        lastInt = x;
+    }
+
+    for (i = firstInt; i < lastInt; i++)
+        result += i;
+
+    return result;
 };
 
 // Do not edit below this line
