c8848115c48e3a31e4601b15a898493f7186d928
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-17 14:44:53 -0600

    exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,30 @@
-const removeFromArray = function() {
+function removeFromArray() {
+    let args = [];
+    let inputArray = [];
+    let currentElement;
+    let currentIndex = 0;
 
+    inputArray = arguments[0];
+
+    for (let i=1; i < arguments.length; ++i)
+        args[i] = arguments[i];
+
+    for (let i=0; i < args.length; ++i) {
+        currentElement = args[i];
+        currentIndex = inputArray.indexOf(currentElement);
+        if (currentIndex > 0)
+            inputArray.slice(currentIndex, 1);
+    }
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/*
+get all args after the first and put them in an array
+loop through the elements of the arg array
+    if current arg array element exists in inputArray AND is correct type
+        splice input array at that index
+    else
+        iterate to next arg element
+*/
