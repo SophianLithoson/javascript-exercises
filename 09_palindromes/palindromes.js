05244ec350b092a8690ef3dcc3b535630e500ff5
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-25 16:18:03 -0600

    fixed syntax and added missing return

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,12 +2,14 @@ const palindromes = function (text) {
     let textNoPuncs = text.replace(/[\.,?!]/g, "");
     let textArray = Array.from(textNoPuncs.toUpperCase());
         
-    for (let i=0, let j=(textArray.length - 1); i <= j; i++, j--) {
+    for (let i=0, j=(textArray.length - 1); i <= j; i++, j--) {
         if (textArray[i] === textArray[j])
             continue;
         else
             return false;
     }
+
+    return true;
 };
 
 // Do not edit below this line
