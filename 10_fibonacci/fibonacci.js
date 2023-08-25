e1d85156d3ed4ff47674003662057e7212641b52
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-25 17:01:14 -0600

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof(num) === String)
+        num = Number(num);
+
+    if (num < 1)
+        return "OOPS";
+
+    let stack = [0, 1];
+
+    for (i=1, i <= num, i++) {
+        stack.pop(stack[0] + stack [1]);
+        stack.shift();
+    }
+
 
 };
 
