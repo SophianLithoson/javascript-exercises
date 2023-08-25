8a3f27f90fbcbd6d62bb026e9d46c98539650d5e
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-25 16:13:31 -0600

    palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let textNoPuncs = text.replace(/[\.,?!]/g, "");
+    let textArray = Array.from(textNoPuncs.toUpperCase());
+        
+    for (let i=0, let j=(textArray.length - 1); i <= j; i++, j--) {
+        if (textArray[i] === textArray[j])
+            continue;
+        else
+            return false;
+    }
 };
 
 // Do not edit below this line
