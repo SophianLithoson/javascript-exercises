172818d0f40befa535169f4d68a3f2fa7f35bd8b
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-25 18:16:34 -0600

    passes first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestFirst = people.sort((a, b) => {
+        const currentYear =  new Date().getFullYear();
+        if (((!a.yearOfDeath) ? currentYear : a.yearOfDeath) - a.yearOfBirth < ((!b.yearOfDeath) ? currentYear : b.yearOfDeath) - b.yearOfBirth)
+            return 1;
+        else
+            return -1;
 
+    });
+
+    return oldestFirst[0];
 };
 
 // Do not edit below this line
